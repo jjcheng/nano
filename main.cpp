fb49b2523c8184ac7419a16c01b6dd612729bfe3
jjcheng <chengjianjin@gmail.com>
2025-03-06 22:53:58 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -391,11 +391,11 @@ bool initModel() {
         return false;
     }
     // Set detection thresholds.
-    CVI_TDL_SetModelThreshold(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, MODEL_THRESH);
-    CVI_TDL_SetModelNmsThreshold(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, MODEL_NMS_THRESH);
+    CVI_TDL_SetModelThreshold(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, 0.5);
+    CVI_TDL_SetModelNmsThreshold(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, 0.5);
     printf("YOLOv8 parameters setup success!\n");
     // Open the model
-    ret = CVI_TDL_OpenModel(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, "yolov8n_int8_2_class.cvimodel");
+    ret = CVI_TDL_OpenModel(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, "/root/yolov8n_int8_2_class.cvimodel");
     if (ret != CVI_SUCCESS) {
         printf("Open model failed with %#x!\n", ret);
         return false;
